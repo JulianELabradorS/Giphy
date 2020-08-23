25002be539c81925334f1541b25e827bcdaa3d8f
Julian Labrador <julian@nabica.co>
2020-08-22 20:04:36 -0500

    proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,94 @@
+window.onload = () => {
+	new NocturneMode();
+	new Slider();
+};
+// MODE HANDLER
+class NocturneMode {
+	constructor() {
+		this.modeButton = document.getElementById('page--mode');
+		this.body = document.getElementsByTagName('body');
+		this.addEvents();
+	}
+	addEvents() {
+		this.modeButton.addEventListener('click', () => {
+			this.changeMode();
+		});
+	}
+
+	changeMode() {
+		event.preventDefault();
+		let hasClass = this.body[0].classList.toggle('nocturne');
+		if (hasClass) {
+			event.currentTarget.innerHTML = 'MODO DIURNO';
+		} else {
+			event.currentTarget.innerHTML = 'MODO NOCTURNO';
+		}
+	}
+}
+//SLIDER HANDLER
+class Slider {
+	constructor() {
+		this.buttonNext = document.getElementById('trending__gifs__slider__next');
+		this.buttonPrev = document.getElementById('trending__gifs__slider__prev');
+		this.sliderWrapper = document.getElementById('trending__gifs__slider__wrapper');
+		this.sliderInner = document.getElementById('trending__gifs__slider__inner');
+		this.sliderInnerWidth;
+		this.maxMovement;
+		this.position = 0;
+		this.addEvents();
+	}
+	addEvents() {
+		this.buttonNext.addEventListener('click', () => {
+			this.slideNext();
+		});
+		this.buttonPrev.addEventListener('click', () => {
+			this.slidePrev();
+		});
+	}
+	slideNext() {
+		this.buttonPrev.classList.add('show');
+		this.sliderInnerWidth = this.getSliderInnerWidth();
+		let movement = this.getMovement();
+		let sliderWrapperWidth = this.sliderWrapper.offsetWidth;
+		this.maxMovement = this.sliderInnerWidth - sliderWrapperWidth;
+		this.position += movement;
+		if (this.maxMovement > this.position) {
+			this.sliderInner.style.right = `${this.position}px`;
+		} else {
+			this.position = this.maxMovement;
+			this.sliderInner.style.right = `${this.position}px`;
+			this.buttonNext.classList.add('hide');
+		}
+	}
+	slidePrev() {
+		this.buttonNext.classList.remove('hide');
+		let movement = this.getMovement();
+		this.position -= movement;
+		console.log(this.position);
+		if (this.position > 0) {
+			this.sliderInner.style.right = `${this.position}px`;
+		} else {
+			this.sliderInner.style.right = `0`;
+			this.buttonPrev.classList.remove('show');
+		}
+	}
+	getSliderInnerWidth() {
+		let items = this.sliderInner.childElementCount;
+		let width;
+		if (screen.width > 768) {
+			width = items * 357 + (items - 1) * 29;
+			return width;
+		} else {
+			width = items * 243;
+			console.log(width);
+			return width;
+		}
+	}
+	getMovement() {
+		if (screen.width > 768) {
+			return 387;
+		} else {
+			return 243;
+		}
+	}
+}
