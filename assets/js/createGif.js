3201018493cf87f82aaad8ac95bf02dffd74faaa
Julian Labrador <lockdownlab@gmail.com>
2020-08-30 14:25:30 -0500

    ajuste 2

--- assets/js/createGif.js
+++ assets/js/createGif.js
@@ -3,31 +3,33 @@ let buttonInitVideo = document.getElementById('create__gif__button');
 let buttonRecordingVideo = document.getElementById('recording__gif__button');
 let createContainer = document.getElementById('create__gif');
 let textCreateGif = document.getElementById('text_createGif');
-let videoContainer = document.getElementById('create__gif__content__video');
+let videoContainer = document.getElementById('stream__video');
 
 headerCreateButton.addEventListener('click', () => {
-	showCreate();
+  showCreate();
 });
 buttonInitVideo.addEventListener('click', () => {
-	initVideo();
+  initVideo();
 });
 
 const showCreate = () => {
-	event.preventDefault();
-	textCreateGif.innerHTML = '';
-	createContainer.classList.add('show--flex');
-	headerCreateButton.classList.add('active');
-	buttonInitVideo.classList.remove('hide');
-	if (favorites.classList.length) {
-		favorites.classList.remove('show');
-		headerFavButton.classList.remove('active--button');
-	} else if (myGifosContainer.classList.length) {
-		myGifosContainer.classList.remove('show');
-		headerMyGifosButton.classList.remove('active--button');
-	} else {
-		sectionPrincipal.classList.add('hide');
-	}
-	let template = `	<h1>
+  event.preventDefault();
+  textCreateGif.innerHTML = '';
+  textCreateGif.classList.remove('hide');
+  createContainer.classList.add('show--flex');
+  headerCreateButton.classList.add('active');
+  buttonInitVideo.classList.remove('hide');
+  buttonRecordingVideo.classList.remove('show');
+  if (favorites.classList.length) {
+    favorites.classList.remove('show');
+    headerFavButton.classList.remove('active--button');
+  } else if (myGifosContainer.classList.length) {
+    myGifosContainer.classList.remove('show');
+    headerMyGifosButton.classList.remove('active--button');
+  } else {
+    sectionPrincipal.classList.add('hide');
+  }
+  let template = `	<h1>
     Aquí podrás <br />
     crear tus propios <span>GIFOS</span>
 </h1>
@@ -35,39 +37,47 @@ const showCreate = () => {
     ¡Crea tu GIFO en sólo 3 pasos! <br />
     (sólo necesitas una cámara para grabar un video)
 </p>`;
-	textCreateGif.insertAdjacentHTML('beforeend', template);
+  textCreateGif.insertAdjacentHTML('beforeend', template);
 };
 
 const initVideo = () => {
-	buttonInitVideo.classList.add('hide');
-	if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-		navigator.mediaDevices
-			.getUserMedia({
-				video: {
-					width: {
-						min: 1024,
-					},
-					height: {
-						min: 800,
-					},
-				},
-			})
-			.then((stream) => {
-				buttonRecordingVideo.classList.add('show');
-				videoContainer.srcObject = stream;
-				videoContainer.play();
-			})
-			.catch(() => {
-				textCreateGif.innerHTML = '';
-				let template = `	<h1>
+  buttonInitVideo.classList.add('hide');
+  if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          width: 646,
+          height: 348,
+        },
+      })
+      .then((stream) => {
+        textCreateGif.classList.add('hide');
+        buttonRecordingVideo.classList.add('show');
+        videoContainer.srcObject = stream;
+        videoContainer.play();
+        buttonRecordingVideo.addEventListener('click', () => {
+          recordingVideo();
+        });
+      })
+      .catch(() => {
+        textCreateGif.innerHTML = '';
+        let template = `	<h1>
                Lo Sentimos, <br />
                Parece que no tienes una camara conectada </span>
             </h1>
             <p>
                 Conectala e intenta de nuevo!
             </p>`;
-				textCreateGif.insertAdjacentHTML('beforeend', template);
-				buttonInitVideo.classList.remove('hide');
-			});
-	}
+        textCreateGif.insertAdjacentHTML('beforeend', template);
+        buttonInitVideo.classList.remove('hide');
+      });
+  }
+};
+const recordingVideo = async () => {
+  let x = videoContainer.srcObject;
+  let y = x.getTracks();
+  y.forEach((element) => {
+    element.stop();
+  });
 };
--- assets/js/myGifos.js
+++ assets/js/myGifos.js
@@ -2,20 +2,25 @@ let headerMyGifosButton = document.getElementById('header__myGifos__button');
 let myGifosContainer = document.getElementById('myGifos');
 
 headerMyGifosButton.addEventListener('click', () => {
-	showMyGifos();
+  showMyGifos();
 });
 
 const showMyGifos = () => {
-	event.preventDefault();
-	myGifosContainer.classList.add('show');
-	headerMyGifosButton.classList.add('active--button');
-	if (favorites.classList.length) {
-		favorites.classList.remove('show');
-		headerFavButton.classList.remove('active--button');
-	} else if (createContainer.classList.length) {
-		createContainer.classList.remove('show--flex');
-		headerCreateButton.classList.remove('active');
-	} else {
-		sectionPrincipal.classList.add('hide');
-	}
+  event.preventDefault();
+  myGifosContainer.classList.add('show');
+  headerMyGifosButton.classList.add('active--button');
+  if (favorites.classList.length) {
+    favorites.classList.remove('show');
+    headerFavButton.classList.remove('active--button');
+  } else if (createContainer.classList.length) {
+    createContainer.classList.remove('show--flex');
+    headerCreateButton.classList.remove('active');
+  } else {
+    sectionPrincipal.classList.add('hide');
+  }
+  revokepermissions();
+};
+const revokepermissions = async () => {
+  const videoPermision = await navigator.permissions.query({ name: 'camera' });
+  console.log(videoPermision);
 };
