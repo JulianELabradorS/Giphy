ed8ab40165e9fe2fa4e7305fa700e44178b27a19
Julian Labrador <julian@nabica.co>
2020-08-30 11:35:22 -0500

    proyecto en fase final falta crear gif

---
+++ assets/js/myGifos.js
@@ -0,0 +1,21 @@
+let headerMyGifosButton = document.getElementById('header__myGifos__button');
+let myGifosContainer = document.getElementById('myGifos');
+
+headerMyGifosButton.addEventListener('click', () => {
+	showMyGifos();
+});
+
+const showMyGifos = () => {
+	event.preventDefault();
+	myGifosContainer.classList.add('show');
+	headerMyGifosButton.classList.add('active--button');
+	if (favorites.classList.length) {
+		favorites.classList.remove('show');
+		headerFavButton.classList.remove('active--button');
+	} else if (createContainer.classList.length) {
+		createContainer.classList.remove('show--flex');
+		headerCreateButton.classList.remove('active');
+	} else {
+		sectionPrincipal.classList.add('hide');
+	}
+};
